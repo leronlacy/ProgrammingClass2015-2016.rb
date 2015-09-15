987b4b2605dab1ba8a14d3a8cd240327185ccddc
leron <llacy@chicagotechacademy.org>
2015-09-15 10:44:19 -0500

    Update README.rb

--- README.rb
+++ README.rb
@@ -1,8 +1,5 @@
 # ProgrammingClass2015-2016.rb
-#hot wings 
-=begin 
-are very good and very tasty 
-=end 
-print "candy corn"
-puts "ups downs" 
+
 my_name = "Leron/Casper".upcase
+my_num = 16
+Cta = true
